13131466795c469c73ef45a31dbe4787d0f2e240
Akrati Bhadauria <122599551+Akrati00@users.noreply.github.com>
2024-10-10 17:21:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
+// export default function App() {
+//   async function getAdvice() {
+//     const res = await fetch("https://api.adviceslip.com/advice");
+//     const data = await res.json();
+//     console.log(data.slip.advice);
+//   }
+//   return (
+//     <div>
+//       <h1>Hello World!</h1>
+//       <button onClick={getAdvice}>Get advice</button>
+//     </div>
+//   );
+// }
+
+import { useState } from "react";
+
 export default function App() {
+  const [advice, setAdvice] = useState("");
   async function getAdvice() {
     const res = await fetch("https://api.adviceslip.com/advice");
     const data = await res.json();
-    console.log(data.slip.advice);
+    setAdvice(data.slip.advice);
   }
   return (
     <div>
-      <h1>Hello World!</h1>
+      <h1>{advice}</h1>
       <button onClick={getAdvice}>Get advice</button>
     </div>
   );

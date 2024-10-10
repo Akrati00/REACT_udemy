7bfb54e3fb0b9cc1f29986b05e336435a6ad5ac4
Akrati Bhadauria <122599551+Akrati00@users.noreply.github.com>
2024-10-10 16:35:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ export default function App() {
   async function getAdvice() {
     const res = await fetch("https://api.adviceslip.com/advice");
     const data = await res.json();
-    console.log(data);
+    console.log(data.slip.advice);
   }
   return (
     <div>
